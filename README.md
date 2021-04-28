02b66bdd06552975c2a4f8ff0a152485a8728bc0
alejandroS <ottoalejandrobonilla2014@gmail.com>
2021-04-28 11:49:19 -0600

    First Commit for Version 2

--- README.md
+++ README.md
@@ -1,2 +1,141 @@
-# FingerPrint_Android
-Package Java for Finger Print Reader
+
+<!-- PROJECT LOGO -->
+<br />
+<p align="center">
+  <a href="https://github.com/alejandrosazo/fingerprint-reader-uareu-android">
+    <img src="images/fingerprint.png" alt="Logo" width="150" height="150">
+  </a>
+
+  <h3 align="center">FingerPrint Reader for Android - uareu</h3>
+
+  <p align="center">
+    Android application to scan fingerprint with the library U.are.U 4500
+  </p>
+</p>
+
+
+
+<!-- TABLE OF CONTENTS -->
+<details open="open">
+  <summary>Table of Contents</summary>
+  <ol>
+    <li>
+      <a href="#about-the-project">About The Project</a>
+    </li>
+    <li>
+      <a href="#getting-started">Getting Started</a>
+    </li>
+    <li><a href="#usage">Usage</a></li>
+    <li><a href="#screenshots">ScreenShots</a></li>
+    <li><a href="#drawbacks">Drawbacks</a></li>
+    <li><a href="#contact">Contact</a></li>
+    <li><a href="#acknowledgements">Acknowledgements</a></li>
+  </ol>
+</details>
+
+
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+
+The project is a simple application, created for Android using Java as the language.
+
+Its main functionality is to scan a person's fingerprint through a "digitalPersonal" reader, specifically the U.are.U 4500 model. 
+
+
+
+<!-- GETTING STARTED -->
+## Getting Started
+
+To use the library in your own project, keep these main lines of code in mind. 
+
+
+
+* Enter this maven repository in your project `build.gradle`
+  ```Gradle
+  // .. rest of your code 
+  allprojects {
+    // .. rest of your code
+    repositories {
+      // .. rest of your code
+        maven { url 'https://dl.bintray.com/shodgson/uareu4500reader' } //<--- this line is important 
+        google()
+        jcenter()
+    }
+  }
+  ```
+
+* Enter this code block in your app `build.gradle`
+  ```Gradle
+  dependencies {
+    // ..rest of your dependencies
+
+    implementation fileTree(dir: 'libs', include: ['*.jar'])
+    testImplementation 'junit:junit:4.+'
+    implementation 'asia.kanopi.tools:fingerscan:0.1'
+    implementation 'com.gemalto.wsq:wsq-android:1.2'
+  }
+  ```
+  _Remember the app build.gradle and the project build.gradle are two different files[]()_
+
+
+
+<!-- USAGE EXAMPLES -->
+## Usage
+
+The application is simple, it only has six components: 
+  * `ivFingerDisplay`: Displays the captured fingerprint image.
+  * `tvStatus`: Fingerprint reader current status message.
+  * `tvError`: Error message in some process.
+  * `tvMessage`: Process status message.
+  * `buttonScan`: Button to start the scanning process.
+  * `base64View`: Displays the base64 of the scanned image.
+
+In the `MainActivity.java` there are 3 methods:
+ 
+ 1. <strong>startScan:</strong>
+  
+    Call the library instance `asia.kanopi.fingerscan.Fingerprint`
+
+ 2. <strong>updateHandler:</strong>
+
+    Updates the state of the physical fingerprint reader, the state depends on the actions that the printHandler method will perform.
+
+ 3. <strong>printHandler:</strong>
+
+    Gets the scanned fingerprint data and returns a ByteArray which is then decoded. 
+
+<!-- SCREENSHOTS -->
+## ScreenShots
+<p align="center">
+<img src="images/Screenshot1.jpeg"  width=200 title="Main Screen">
+<img src="images/Screenshot2.jpeg"  width=200 title="No device Connect">
+<img src="images/Screenshot3.jpeg"  width=200 title="Device Detected">
+<img src="images/Screenshot4.jpeg"  width=200 title="Ready for Scan">
+<img src="images/Screenshot5.jpeg"  width=200 title="FingerPrint Scanned">
+</p>
+
+<!-- DRAWBACKS -->
+## Drawbacks
+
+* The application requires the "digitalPersonal" reader to be connected via an OTG cable. Therefore, every time you want to test, you must disconnect the phone and the debugging will stop.
+
+* It is not possible to connect the fingerprint reader to the computer and test it through the Android Studio emulator. If you find any way to do this, leave a suggestion on how to do it. 
+
+<!-- CONTACT -->
+## Contact
+
+Alejandro Sazo - ottoalejandrobonilla2014@gmail.com
+
+Project Link: [https://github.com/alejandrosazo/fingerprint-reader-uareu-android](https://github.com/alejandrosazo/fingerprint-reader-uareu-android)
+
+
+
+<!-- ACKNOWLEDGEMENTS -->
+## Acknowledgements
+Special thanks to Steven Hodgson steven@kanopi.asia.
+
+The base code can be found in the following repository:
+* [uareu](https://github.com/shodgson/uareu)
+
+[screenshot1]: images/Screenshot1.jpeg
--- app/src/main/java/com/example/fingerprintexample/MainActivity.java
+++ app/src/main/java/com/example/fingerprintexample/MainActivity.java
@@ -1,27 +1,39 @@
-package com.example.fingerprintexample;
+ package com.example.fingerprintexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Looper;
+import android.os.Message;
 import android.util.Base64;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-import asia.kanopi.fingerscan.Status;
 import com.gemalto.wsq.WSQEncoder;
 
-public class MainActivity extends AppCompatActivity {
+import asia.kanopi.fingerscan.Status;
+import android.os.Handler;
+import asia.kanopi.fingerscan.Fingerprint;
 
+
+import java.io.ByteArrayOutputStream;
+
+ public class MainActivity extends AppCompatActivity {
+
+
+    /// ### Declaration of variables ### ///
     ImageView ivFinger;
     TextView tvMessage;
     TextView base64View;
+    private TextView tvStatus;
+    private TextView tvError;
     byte[] img;
     Bitmap bm;
     String convertBase64 = null;
-    private static final int SCAN_FINGER = 0;
+    private Fingerprint fingerprint;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,43 +42,99 @@ public class MainActivity extends AppCompatActivity {
         tvMessage = (TextView) findViewById(R.id.tvMessage);
         ivFinger = (ImageView) findViewById(R.id.ivFingerDisplay);
         base64View = (TextView) findViewById(R.id.base64View);
+        tvStatus = (TextView) findViewById(R.id.tvStatus);
+        tvError = (TextView) findViewById(R.id.tvError);
+
+        fingerprint = new Fingerprint();
     }
+
 
     public void startScan(View view) {
-        Intent intent = new Intent(this, ScanActivity.class);
-        startActivityForResult(intent, SCAN_FINGER);
+        fingerprint.scan(this, printHandler, updateHandler);
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        int status;
-        String errorMesssage;
 
-        switch (requestCode) {
-            case (SCAN_FINGER): {
-                if (resultCode == RESULT_OK) {
-                    status = data.getIntExtra("status", Status.ERROR);
-                    if (status == Status.SUCCESS) {
-                        tvMessage.setText("Fingerprint captured");
-                        img = data.getByteArrayExtra("img");
-                        bm = BitmapFactory.decodeByteArray(img, 0, img.length);
+     Handler updateHandler = new Handler(Looper.getMainLooper()) {
+         @Override
+         public void handleMessage(Message msg) {
+             int status = msg.getData().getInt("status");
+             tvError.setText("Error: ");
+             switch (status) {
+                 case Status.INITIALISED:
+                     tvStatus.setText("Status: " +"Setting up reader");
+                     break;
+                 case Status.SCANNER_POWERED_ON:
+                     tvStatus.setText("Status: " +"Reader powered on");
+                     break;
+                 case Status.READY_TO_SCAN:
+                     tvStatus.setText("Status: " +"Ready to scan finger");
+                     break;
+                 case Status.FINGER_DETECTED:
+                     tvStatus.setText("Status: " +"Finger detected");
+                     break;
+                 case Status.RECEIVING_IMAGE:
+                     tvStatus.setText("Status: " +"Receiving image");
+                     break;
+                 case Status.FINGER_LIFTED:
+                     tvStatus.setText("Status: " +"Finger has been lifted off reader");
+                     break;
+                 case Status.SCANNER_POWERED_OFF:
+                     tvStatus.setText("Status: " +"Reader is off");
+                     break;
+                 case Status.SUCCESS:
+                     tvStatus.setText("Status: " +"Fingerprint successfully captured");
+                     break;
+                 case Status.ERROR:
+                     tvStatus.setText("Status: " +"Error");
+                     tvError.setText("Error: " +msg.getData().getString("errorMessage"));
+                     break;
+                 default:
+                     tvStatus.setText("Status: " +String.valueOf(status));
+                     tvError.setText("Error: " +msg.getData().getString("errorMessage"));
+                     break;
+             }
+         }
+     };
 
-//higher-quality encode
-                      byte[] wsqData = new WSQEncoder(bm)
-                                .setBitrate(WSQEncoder.BITRATE_5_TO_1)
-                                .encode();
-                        convertBase64 = Base64.encodeToString(wsqData, Base64.DEFAULT);
 
-                        base64View.setText(convertBase64); //Data Import
-                        ivFinger.setImageBitmap(bm); //Data Import
-                    } else {
-                        errorMesssage = data.getStringExtra("errorMessage");
-                        tvMessage.setText("-- Error: " + errorMesssage + " --");
-                    }
-                }
-                break;
-            }
-        }
-    }
+     Handler printHandler = new Handler(Looper.getMainLooper()) {
+         @Override
+         public void handleMessage(Message msg) {
+
+             String errorMessage = "empty";
+             int status = msg.getData().getInt("status");
+
+             if (status == Status.SUCCESS) {
+                 tvMessage.setText("Message: " +"Fingerprint captured");
+
+                 img = msg.getData().getByteArray("img");
+                 bm = BitmapFactory.decodeByteArray(img, 0, img.length);
+
+                 ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                 bm.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+
+                 byte[] byteArray = byteArrayOutputStream .toByteArray();
+
+                 /// ### encoded variable contain the base64 of the fingerprint ### ///
+                 String encoded = Base64.encodeToString(byteArray, Base64.DEFAULT);
+
+                 /// ### Higher-quality Encode - imageAsBytes is important for display Image of the fingerprint ### ///
+                 byte[] imageAsBytes = Base64.decode(encoded.getBytes(), Base64.DEFAULT);
+
+                 /// ### wsqData is optional data for codification in WSQ ### ///
+                 byte[] wsqData = new WSQEncoder(bm)
+                         .setBitrate(WSQEncoder.BITRATE_5_TO_1)
+                         .encode();
+                 convertBase64 = Base64.encodeToString(wsqData, Base64.DEFAULT);
+
+                 /// ### Display data in layout ### ///
+                 base64View.setText("Base64: " + encoded);
+                 ivFinger.setImageBitmap(BitmapFactory.decodeByteArray(imageAsBytes, 0, imageAsBytes.length));
+
+             } else {
+                 errorMessage = msg.getData().getString("errorMessage");
+                 tvMessage.setText("-- Error: " + errorMessage + " --");
+             }
+         }
+     };
 }
